d90d1edf7605d82620804c01a18b20089a7394d8
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-08-17 00:00:50 -0300

    Backend

---
+++ config/database.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: env.bool("DATABASE_SSL"),
+    },
+    pool: {
+      min: env.int("DATABASE_POOL_MIN"),
+      max: env.int("DATABASE_POOL_MAX"),
+    }
+  },
+});
